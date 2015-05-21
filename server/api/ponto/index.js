24cd2bd889c6cb0762c7f69366b2810e12b10049
Rodrigo <rodrigopsschweitzer@gmail.com>
2015-05-20 23:45:15 -0300

    Inserida autenticação para as rotas de /ponto

--- server/api/ponto/index.js
+++ server/api/ponto/index.js
@@ -2,16 +2,17 @@
 
 var express = require('express');
 var controller = require('./ponto.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/total', controller.total);
-router.get('/banco', controller.banco);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/total', auth.isAuthenticated(), controller.total);
+router.get('/banco', auth.isAuthenticated(), controller.banco);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
